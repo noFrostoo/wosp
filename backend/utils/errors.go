451efa50fcf76ffc974267a7788ad6c40b7268c1
Bart <lewandowskib.sz@gmail.com>
2024-02-20 23:41:14 +0100

    finalize docker builder CI

--- backend/utils/errors.go
+++ backend/utils/errors.go
@@ -12,6 +12,7 @@ type Error struct {
 func NewError(err error) Error {
 	e := Error{}
 	e.Errors = make(map[string]interface{})
+	fmt.Print("test")
 
 	switch v := err.(type) {
 		case *echo.HTTPError:
