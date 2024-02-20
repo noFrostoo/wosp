d4ba7f52daf71f620da1ca3e24c4910e533a5ae8
Bart <lewandowskib.sz@gmail.com>
2024-02-20 19:59:20 +0100

    docker_CI

--- scripts/build-frontend-push.sh
+++ scripts/build-frontend-push.sh
@@ -3,8 +3,5 @@
 ## !! Run this script in root of the project
 #######
 
-docker build -t nofrostoo/todo-app:dev ./backend
-docker push nofrostoo/todo-app:dev
-
 docker build -t nofrostoo/todo-app-frontend:dev ./frontend
 docker push nofrostoo/todo-app-frontend:dev
